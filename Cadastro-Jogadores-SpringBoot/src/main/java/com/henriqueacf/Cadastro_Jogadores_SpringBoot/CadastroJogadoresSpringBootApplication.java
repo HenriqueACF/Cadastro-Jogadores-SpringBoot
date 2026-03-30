f98e6ecf685c585e60753874ea82f10e301ba49d
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2026-03-30 20:58:24 -0300

    first commit

---
+++ Cadastro-Jogadores-SpringBoot/src/main/java/com/henriqueacf/Cadastro_Jogadores_SpringBoot/CadastroJogadoresSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.henriqueacf.Cadastro_Jogadores_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroJogadoresSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroJogadoresSpringBootApplication.class, args);
+	}
+
+}
---
+++ Cadastro-Jogadores-SpringBoot/src/test/java/com/henriqueacf/Cadastro_Jogadores_SpringBoot/CadastroJogadoresSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriqueacf.Cadastro_Jogadores_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroJogadoresSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
