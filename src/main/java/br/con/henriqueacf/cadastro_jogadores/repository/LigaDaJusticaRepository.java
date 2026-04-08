438f782e95252aa0e29f2782956fa6d85610dcac
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2026-04-08 14:42:06 -0300

    feat: finalizando repository de liga da justiça

--- src/main/java/br/con/henriqueacf/cadastro_jogadores/repository/LigaDaJusticaRepository.java
+++ src/main/java/br/con/henriqueacf/cadastro_jogadores/repository/LigaDaJusticaRepository.java
@@ -1,4 +1,24 @@
 package br.con.henriqueacf.cadastro_jogadores.repository;
 
+import br.con.henriqueacf.cadastro_jogadores.model.GrupoCodinome;
+import br.con.henriqueacf.cadastro_jogadores.web.LigaDaJusticaDTO;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
 public class LigaDaJusticaRepository implements CodinomeRepository{
+    @Override
+    public List<String> buscarCodinomes() throws Exception {
+        var codinomes =  RestClient.builder()
+                .baseUrl(GrupoCodinome.LIGA_DA_JUSTICA.getUri())
+                .build()
+                .get()
+                .retrieve()
+                .body(String.class);
+
+        var xmlMapper = new XmlMapper();
+        var ligaDaJustica = xmlMapper.readValue(codinomes, LigaDaJusticaDTO.class);
+        return ligaDaJustica.getCodinomes();
+    }
 }
